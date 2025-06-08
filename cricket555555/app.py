a77ac8fd3d0d6532b6b65414644375060ca6f2e8
Shraddha160 <88272582+Shraddha160@users.noreply.github.com>
2025-06-08 18:38:58 +0530

    Update app.py

--- cricket555555/app.py
+++ cricket555555/app.py
@@ -468,4 +468,6 @@ def process_video():
         return jsonify({"error": str(e), "status": "error"}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
